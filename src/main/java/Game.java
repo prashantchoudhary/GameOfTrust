0e5a1a5f24244e5bdde1f0bb4e501dcfdbf46f30
prashantchoudhary <get.prashant.007@gmail.com>
2020-04-14 14:49:48 +0530

    Added Successive gameplay and Display

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,21 +1,42 @@
 import javafx.util.Pair;
 
+import java.io.PrintStream;
+
 public class Game {
-
-
+    private Integer round = 0;
     private Player player1;
     private Player player2;
     private RulesEngine rulesEngine;
+    private PrintStream printStream;
 
-    public Game(Player player1, Player player2, RulesEngine rulesEngine) {
+    public Game(Player player1, Player player2, RulesEngine rulesEngine, PrintStream printStream) {
         this.player1 = player1;
         this.player2 = player2;
         this.rulesEngine = rulesEngine;
+        this.printStream = printStream;
     }
 
     public void playOnce() {
-      Pair<Integer,Integer> score=  rulesEngine.getScoreForMoves(player1.getCurrentMove(),player2.getCurrentMove());
-      player1.updateScore(score.getKey());
-      player2.updateScore(score.getKey());
+        updateRound();
+        Pair<Integer, Integer> score = rulesEngine.getScoreForMoves(player1.getCurrentMove(), player2.getCurrentMove());
+        updateScores(score);
+        displayScores();
+    }
+
+    private void updateRound() {
+        round++;
+    }
+
+    private void updateScores(Pair<Integer, Integer> score) {
+        player1.updateScore(player1.getScore() + score.getKey());
+        player2.updateScore(player2.getScore() + score.getKey());
+    }
+
+    private void displayScores() {
+        printStream.println("Round:" + round);
+        printStream.println("Player 1 Score:" + player1.getScore());
+        printStream.println("Player 2 Score:" + player2.getScore());
+        printStream.println();
     }
 }
+
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,10 +9,35 @@ public class GameTest {
         Player player1 = new Player(MoveTypes.Cooperate);
         Player player2 = new Player(MoveTypes.Cooperate);
         RulesEngine rulesEngine = new RulesEngine();
-        Game game = new Game(player1, player2, rulesEngine);
+        Game game = new Game(player1, player2, rulesEngine, System.out);
         game.playOnce();
         assertEquals((Integer) 2, player1.getScore());
         assertEquals((Integer) 2, player2.getScore());
+    }
+
+    @Test
+    public void whenBothPlayersAreCooperatingForTwoRoundsExpectScoreFourEach() {
+        Player player1 = new Player(MoveTypes.Cooperate);
+        Player player2 = new Player(MoveTypes.Cooperate);
+        RulesEngine rulesEngine = new RulesEngine();
+        Game game = new Game(player1, player2, rulesEngine, System.out);
+        game.playOnce();
+        game.playOnce();
+        assertEquals((Integer) 4, player1.getScore());
+        assertEquals((Integer) 4, player2.getScore());
+    }
+
+    @Test
+    public void whenBothPlayersAreCooperatingForFiveRoundsExpectScoreTenEach() {
+        Player player1 = new Player(MoveTypes.Cooperate);
+        Player player2 = new Player(MoveTypes.Cooperate);
+        RulesEngine rulesEngine = new RulesEngine();
+        Game game = new Game(player1, player2, rulesEngine, System.out);
+        for (int i = 0; i < 5; i++) {
+            game.playOnce();
+        }
+        assertEquals((Integer) 10, player1.getScore());
+        assertEquals((Integer) 10, player2.getScore());
     }
 
 }
